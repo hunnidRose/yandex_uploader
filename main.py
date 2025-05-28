983f24786d9602c16d5d100e60b8f73f3a79049d
hunnidRose <hunnidrose@gmail.com>
2025-05-28 22:15:43 +0400

    Wrote the main script of the program

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ all_breeds_list = response.json()
 # Путем пользовательского ввода получаем название породы
 # и делаем проверку на наличие ее в нашем списке
 breed = input('Название породы(на англ.): ')
-breed_lower = breed.lower()
-if breed_lower in all_breeds_list['message']:
+if breed.lower() in all_breeds_list['message']:
     print('Порода успешно найдена', end='\n\n')
 else:
     raise KeyError(f"Порода '{breed}' не найдена")
@@ -32,3 +31,50 @@ if status_code == 200:
     print('Подключение установлено', end='\n\n')
 else:
     raise ConnectionError(response.json()['message'])
+
+# Создаем папку на Яндекс.Диске
+params = {
+    'path': breed.capitalize()
+}
+headers['Content-Type'] = 'application/json'
+response = requests.put(yandex_base_url + '/v1/disk/resources',
+                        params=params, headers=headers)
+
+# Получаем список из названий файлов, находящихся в папке,
+# чтобы в дальнейшем избежать загрузки одинаковых изображений
+params = {
+    'path': f'{breed.capitalize()}/'
+}
+del headers['Content-Type']
+response = requests.get(yandex_base_url + '/v1/disk/resources',
+                        params=params, headers=headers)
+folder_items = response.json()['_embedded']['items']
+folder_file_names = []
+for item in folder_items:
+    folder_file_names.append(item['name'])
+
+# Загружаем случайное изображение породы в созданную папку
+headers['Content-Type'] = 'application/json'
+dog_base_url = f'https://dog.ceo/api/breed/{breed.lower()}/'
+if all_breeds_list['message'][breed.lower()]:
+    for sub_breed in all_breeds_list['message'][breed.lower()]:
+        response = requests.get(f'{dog_base_url}{sub_breed}/images/random')
+        image_url = response.json()['message']
+        image_name = f'{sub_breed}_{breed.lower()}_{image_url.split('/')[-1]}'
+        params = {
+            'path': f'{breed.capitalize()}/{image_name}',
+            'url': image_url
+        }
+        if image_name not in folder_file_names:
+            requests.post(yandex_base_url + '/v1/disk/resources/upload',
+                            params=params, headers=headers)
+else:
+    response = requests.get(f'{dog_base_url}images/random')
+    image_url = response.json()['message']
+    image_name = f'{breed.lower()}_{image_url.split('/')[-1]}'
+    params = {
+        'path': f'{breed.capitalize()}/{image_name}',
+        'url': image_url
+    }
+    requests.post(yandex_base_url + '/v1/disk/resources/upload',
+                  params=params, headers=headers)
